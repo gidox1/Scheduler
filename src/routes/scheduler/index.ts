d1bcb60843e53d9a9365f070cb7f1c1ff7e3f96c
Gideon Odiase <gideon.odiase@shuttlers.ng>
2021-09-05 00:33:27 +0100

    Added Express type to routes

--- src/routes/scheduler/index.ts
+++ src/routes/scheduler/index.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction, Express } from 'express';
 
 export default (app: Express) => {
-  app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send({
+  app.get('/', (req: Request, res: Response, next: NextFunction) : Response => {
+    return res.send({
       message: 'home of scheduler'
     })
   })
